0bb59cabfd732a4cf110c6f7e93350e7c8c3d51a
fervicente8 <fferminvicente@gmail.com>
2024-05-07 14:26:12 -0300

    configuro astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import amplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
