01182ee36fe1e5bef150bc8b95ee2a351455f9e8
fervicente8 <fferminvicente@gmail.com>
2024-05-07 14:43:46 -0300

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import amplify from "astro-aws-amplify";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: amplify()
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
